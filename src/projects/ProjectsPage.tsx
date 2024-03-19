013a3048eed3d3b90aed4eab17f8dd5f51793b34
tpuskarevic <tpuskarevic@quanterix.com>
2024-03-19 15:24:22 -0400

    Up to Lab 19

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -11,6 +11,11 @@ function ProjectsPage() {
     const [projects, setProjects] = useState<Project[]>([]);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState<string | undefined>(undefined);
+    const [currentPage, setCurrentPage] = useState(1);
+
+    const handleMoreClick = () => {
+      setCurrentPage((currentPage) => currentPage + 1);
+    };
 
 // Approach 1: using promise then
 //  useEffect(() => {
@@ -36,10 +41,15 @@ function ProjectsPage() {
         async function loadProjects() {
           setLoading(true);
           try {
-            const data = await projectAPI.get(1);
-            setError('');
-           // setProjects(data);
+            const data = await projectAPI.get(currentPage);
+           // setError('');
+
+          if (currentPage ==1){
             setProjects(data);
+          } else{
+            setProjects((project) => [...projects, ...data]);
+          }
+
           }
            catch (e) {
            if (e instanceof Error) {
@@ -50,37 +60,62 @@ function ProjectsPage() {
         }
         }
         loadProjects();
-      }, []);
+  }, [currentPage]);
 
 
 
     const saveProject = (project: Project) => {
         console.log('Saving project: ', project);
-        let updatedProjects = projects.map((p: Project) => {
+/*        let updatedProjects = projects.map((p: Project) => {
             return p.id === project.id ? project: p;
         });
         setProjects(updatedProjects);
+*/
+// invoking the method in the Projects Page component
+      projectAPI 
+      .put(project)
+      .then((updatedProject) => {
+        let updatedProjects = projects.map((p: Project) => {
+          return p.id === project.id ? new Project(updatedProject) :p;
+        });
+        setProjects(updatedProjects);
+      })
+      .catch((e) => {
+        if (e instanceof Error) {
+          setError(e.message);
+        }
+      });
     };
     return (
         <Fragment>
         <h1> Projects</h1>
 
-        {error && (
-          <div className="row">
+       {error && (
+        <div className="row">
           <div className="card large error">
-          <section> 
-          <p>
-            <span className="icon-alert inverse "></span>
-            {error}
-          </p>
-          </section>
+            <section>
+              <p>
+                <span className="icon-alert inverse "></span>
+                {error}
+              </p>
+            </section>
           </div>
-          </div>
-        )}
-
-
+        </div>
+      )}
 
         <ProjectList onSave={saveProject} projects = {projects} />
+
+      {!loading && !error && (
+        <div className="row">
+          <div className="col-sm-12">
+            <div className="button-group fluid">
+              <button className="button default" onClick={handleMoreClick}>
+                More...
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
 
         {loading && (
             <div className="center-page">
